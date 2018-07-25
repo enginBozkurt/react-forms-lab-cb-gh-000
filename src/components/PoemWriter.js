a95f28baa974d67d7a5282f90cc57258e17a41bb
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-25 18:46:22 +0000

    automatically backed up by learn

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,7 +4,10 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+     poem: '',
+     hidden: false
+   };
   }
 
   render() {
