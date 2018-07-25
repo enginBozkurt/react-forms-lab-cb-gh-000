7094d1c408e977d71f267abe3d3212a3e49d5938
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-25 19:00:21 +0000

    automatically backed up by learn

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -34,8 +34,8 @@ class PoemWriter extends React.Component {
   render() {
     return (
       <div>
-        <textarea 
-        rows="3" cols="60" 
+        <textarea
+        rows="3" cols="60"
         name="poem"
         value={this.state.poem}
         onChange={this.handleChange}
